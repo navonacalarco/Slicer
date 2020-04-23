78ef9eb52f939d2c452dfbe21831d6f0404fc648
ɐuoʌɐu <navona.calarco@camh.ca>
2020-04-22 23:23:24 -0400

    Update 07_applyAtlas.sh

--- 07_applyAtlas.sh
+++ 07_applyAtlas.sh
@@ -10,19 +10,14 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-445
 
-### mem-per-cpu went up from 18432 to 36864
-### cpus-per-task went down from 2 to 1
-### and array went from 1-445%20 to 1-445
-### the command for updating this in the live job was:
+### note from Kevin: we can ammend a live slurm job (i.e., all still-PENDING array tasks) as follows:
 #### scontrol update jobid=309759 minmemorycpu=36864 cpuspertask=1 mincpusnode=1 arraytaskthrottle=0
-### Running a command like this will give an error, but
-### this is only because it cannot update currently live
-### jobs; all the still-PENDING array tasks will be updated.
-### -- kevin 2020-03-02
+### (it will give an error but can disregard; error related to not updating currently running jobs)
 
 #The files we ultimately want for supplementary steps are in /projects/ncalarco/thesis/SPINS/Slicer/data/registered/FiberMeasurements/
 #Read documentation here: https://github.com/SlicerDMRI/whitematteranalysis/wiki/2c)-Running-the-Clustering-Pipeline-to-Cluster-a-Single-Subject-from-the-Atlas
 #Also, see tutorial here: https://github.com/SlicerDMRI/whitematteranalysis/blob/master/doc/subject-specific-tractography-parcellation.md
+
 #Note that there are QC steps for each of these major 6 steps. See https://github.com/navonacalarco/Slicer/master/10_qualityControl.sh
 
 #Submit with sbatch
@@ -52,78 +47,104 @@ declare -a listHemispheres=("tracts_commissural" "tracts_left_hemisphere" "tract
 
 mkdir -p $outputfolder
 
-#STEP 1 OF 6: creates RegisterToAtlas
+#STEP 1 OF 6: creates 01_TractRegistration (formerly RegisterToAtlas)
 #this step registers each subject to the ORG800 atlas
 #note: we are using rigid-affine registration (cf. affine + nonrigid)
 #note: the .tfm file is the transform matrix
-if [ ! -e $outputfolder/RegisterToAtlas/${subject}/output_tractography/${subject}'_reg.vtk' ]; then
+if [ ! -e $outputfolder/01_TractRegistration/${subject}/output_tractography/${subject}'_reg.vtk' ]; then
 wm_register_to_atlas_new.py -mode rigid_affine_fast \
-  $inputfolder $atlas $outputfolder/RegisterToAtlas
+  $inputfolder \
+  $atlas \
+  $outputfolder/01_TractRegistration
 else
   echo "wm_register_to_atlas_new.py was already run on this subject!"
 fi
 
-#STEP 2 OF 6: creates ClusterFromAtlas
+#STEP 2 OF 6: creates FiberClustering/InitialClusters/ (formerly ClusterFromAtlas)
 #then, create clusters - these are .vtp files of each of the n=800 clusters, for each participants
 #note: previously had flag for `-l 20`
-if [ ! -e $outputfolder/ClusterFromAtlas/${subject}'_reg' ]; then
+if [ ! -e $outputfolder/02_FiberClustering/InitialClusters/${subject}'_reg' ]; then
 wm_cluster_from_atlas.py \
-  $outputfolder/RegisterToAtlas/${subject}_eddy_fixed_SlicerTractography/output_tractography/${subject}'_eddy_fixed_SlicerTractography_reg.vtk' \
-  $atlasDirectory $outputfolder/ClusterFromAtlas
+  $outputfolder/01_TractRegistration/${subject}_eddy_fixed_SlicerTractography/output_tractography/${subject}'_eddy_fixed_SlicerTractography_reg.vtk' \
+  $atlasDirectory \
+  $outputfolder/02_FiberClustering/InitialClusters/
 else
   echo "wm_cluster_from_atlas_new.py was already run on this subject!"
 fi
 
-#STEP 3 OF 6: creates OutliersPerSubject
+#STEP 3 OF 6: creates FiberClustering/OutlierRemovedClusters/ (formerly OutliersPerSubject)
 #create a version without any outliers -- this is the same as above (.vtp for n=800), but without outliers
-if [ ! -e $outputfolder/OutliersPerSubject/${subject}'_reg_outlier_removed' ]; then
+#note: we are removing outliers at the 4SD threshold
+if [ ! -e $outputfolder/02_FiberClustering/OutlierRemovedClusters/${subject}'_reg_outlier_removed' ]; then
 wm_cluster_remove_outliers.py \
   -cluster_outlier_std 4 \
-  $outputfolder/ClusterFromAtlas/${subject}'_eddy_fixed_SlicerTractography_reg' \
+  $outputfolder/02_FiberClustering/InitialClusters/${subject}'_eddy_fixed_SlicerTractography_reg' \
   $atlasDirectory \
-  $outputfolder/OutliersPerSubject
+  $outputfolder/02_FiberClustering/OutlierRemovedClusters
 else
   echo "wm_cluster_remove_outliers.py was already run on this subject!"
 fi
 
-#STEP 4 OF 6:creates ClusterByHemisphere
+#NEW STEP: check cluster location 
+#this script assess the hemispheric location (left, right or commissural) of each fiber in each fiber cluster
+#each cluster (the vtp file) is updated by adding additional information about hemisphere location
+#this information is used to separate the clusters after transforming them back to the input tractography space
+wm_assess_cluster_location_by_hemisphere.py \
+   $outputfolder/FiberClustering/OutlierRemovedClusters/${subject}'_reg_outlier_removed' \ 
+   -clusterLocationFile \
+   $atlasDirectory/cluster_hemisphere_location.txt
+
+#NEW STEP: transform fiber locations
+wm_harden_transform.py -i -t \
+   $outputfolder/01_TractRegistration/${subject}_eddy_fixed_SlicerTractography/output_tractography/itk_txform_{subject}_eddy_fixed_SlicerTractography.tfm \
+   ./FiberClustering/OutlierRemovedClusters/${subject}'_reg_outlier_removed' \
+   ./FiberClustering/TransformedClusters/${subject} \
+   /opt/quarantine/slicer/0,nightly  #or maybe no 0?
+
+#STEP 4 OF 6:creates /FiberClustering/SeparatedClusters (formerly ClusterByHemisphere)
 #creates .vtps of all the n=800 tracts by hemisphere (left, right, commissural, even if shouldn't exist, i.e., creates a 'left hemisphere' .vtp for commissural tracts)
-if [ ! -e $outputfolder/ClusterByHemisphere/'OutliersPerSubject_'${subject} ]; then
+if [ ! -e $outputfolder/FiberClustering/SeparatedClusters/'OutliersPerSubject_'${subject} ]; then
 wm_separate_clusters_by_hemisphere.py \
-  -atlasMRML $clusteredmrml \
-  $outputfolder/OutliersPerSubject/${subject}'_eddy_fixed_SlicerTractography_reg_outlier_removed'/ \
-  $outputfolder/ClusterByHemisphere/'OutliersPerSubject_'${subject}
+  #-atlasMRML $clusteredmrml \
+  $outputfolder/FiberClustering/TransformedClusters/${subject} \
+  $outputfolder/FiberClustering/SeparatedClusters/${subject}
 else
   echo "wm_separate_clusters_by_hemisphere.py was already run on this subject!"
 fi
 
-#STEP 5 OF 6: creates AppendClusters
+#STEP 5 OF 6: creates Anatomical tracts (formerly AppendClusters)
 #combines the n=800 fiber bundles into the n=41 named tracts -- again, by hemisphere
-if [ ! -e $outputfolder/AppendClusters/'OutliersPerSubject_'${subject} ]; then
-for hemisphere in "${listHemispheres[@]}"; do
-echo $hemisphere
-while read tractname; do
-wm_append_clusters.py \
-  -appendedTractName $tractname \
-  -tractMRML $atlasDirectory/$tractname'.mrml' \
-  $outputfolder/ClusterByHemisphere/'OutliersPerSubject_'${subject}/$hemisphere \
-  $outputfolder/AppendClusters/'OutliersPerSubject_'${subject}/$hemisphere >> /projects/ncalarco/thesis/SPINS/Slicer/logs/log_wma.txt
-done < $tractsfile
-done
-else
-  echo "wm_append_clusters.py was already run on this subject!"
-fi
+#if [ ! -e $outputfolder/AppendClusters/'OutliersPerSubject_'${subject} ]; then
+#for hemisphere in "${listHemispheres[@]}"; do
+#echo $hemisphere
+#while read tractname; do
+#wm_append_clusters.py \
+#  -appendedTractName $tractname \
+#  -tractMRML $atlasDirectory/$tractname'.mrml' \
+#  $outputfolder/ClusterByHemisphere/'OutliersPerSubject_'${subject}/$hemisphere \
+#  $outputfolder/AppendClusters/'OutliersPerSubject_'${subject}/$hemisphere >> /projects/ncalarco/thesis/SPINS/Slicer/logs/log_wma.txt
+#done < $tractsfile
+#done
+#else
+#  echo "wm_append_clusters.py was already run on this subject!"
+#fi
+
+wm_append_clusters_to_anatomical_tracts.py \
+   $outputfolder/FiberClustering/SeparatedClusters \
+   $atlas \
+   $outputfolder/AnatomicalTracts
+
 
 #STEP 6 OF 6:creates FiberMeasurements
 #this step creates a csv file, again per hemisphere, with key metrics per n=41 named tracts, for each participant
 if [ ! -e $outputfolder/FiberMeasurements/${subject} ]; then
 for hemisphere in "${listHemispheres[@]}"; do
 echo $hemisphere
-mkdir -p $outputfolder/FiberMeasurements/${subject}/$hemisphere/
-echo $outputfolder/FiberMeasurements/${subject}/$hemisphere >> /projects/ncalarco/thesis/SPINS/Slicer/txt_outputs/FiberMeasurements.txt;
+mkdir -p $outputfolder/FiberClustering/SeparatedClusters/${subject}/$hemisphere/
+echo $outputfolder/FiberClustering/SeparatedClusters/${subject}/$hemisphere >> /projects/ncalarco/thesis/SPINS/Slicer/txt_outputs/FiberMeasurements.txt;
 FiberTractMeasurements \
-  --outputfile $outputfolder/FiberMeasurements/${subject}/$hemisphere/${subject}'.csv' \
-  --inputdirectory $outputfolder/AppendClusters/'OutliersPerSubject_'${subject}/$hemisphere \
+  --outputfile $outputfolder/FiberClustering/SeparatedClusters/${subject}/$hemisphere/${subject}'.csv' \
+  --inputdirectory $outputfolder/AnatomicalTracts/'OutliersPerSubject_'${subject}/$hemisphere \
   -i Fibers_File_Folder \
   --separator Tab \
   -f Column_Hierarchy
