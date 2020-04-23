04bff57e24ae2994c14895db41899be345fd5c85
ɐuoʌɐu <navona.calarco@camh.ca>
2020-04-23 16:05:43 -0400

    Update 04_makeNRRDs.sh

--- 04_makeNRRDs.sh
+++ 04_makeNRRDs.sh
@@ -11,9 +11,11 @@ data_dir="/projects/ncalarco/thesis/SPINS/Slicer/data/01_dmriprep"
 #make output 04_nrrd directory
 mkdir -p $output_dir
 
-############################
-#first, convert DWI data
-############################
+####################################################################################
+#Note: Do I need to convert Siemens data to .nrrd differently than non-Siemens? 
+#Fan suggests may want to use --useBMatrixGradientDirections flag
+#reference: http://dmri.slicer.org/tutorials/Slicer-4.8/DWIConverterTutorial.pdf
+####################################################################################
 
 while read id; do
 echo ${id}
@@ -24,26 +26,6 @@ DWIConvert --inputVolume ${input_dir_dwi}/${id}_eddy_fixed.nii.gz \
    --inputBValues ${data_dir}/${id}/*/dwi/${id}_*desc-eddy_dwi.bval \
    --inputBVectors ${data_dir}/${id}/*/dwi/${id}_*desc-eddy_dwi.bvec \
    --outputVolume ${output_dir}/${id}_eddy_fixed.nrrd
-else
-  echo ${id} "is converted to nrrd"
-fi
-done < /projects/ncalarco/thesis/SPINS/Slicer/txt_outputs/03_sublist.txt
-
-############################
-#second, convert masks
-############################
-
-#Confirm I need bvec and bval to convert the mask to .nrrd?
-
-while read id; do
-echo ${id}
-if [ ! -e ${output_dir}/${id}/${id}_mask.nrrd ]; then
-
-DWIConvert --inputVolume ${input_dir_masks}/${id}/*/dwi/*brainsuite_mask.nii.gz 
-   --conversionMode FSLToNrrd \
-   --inputBValues ${data_dir}/${id}/*/dwi/${id}_*desc-eddy_dwi.bval \
-   --inputBVectors ${data_dir}/${id}/*/dwi/${id}_*desc-eddy_dwi.bvec \
-   --outputVolume ${output_dir}/${id}_mask.nrrd 
 else
   echo ${id} "is converted to nrrd"
 fi
