58dfb2995d59cca6ff49de1392b249cb8a2d358d
ɐuoʌɐu <navona.calarco@camh.ca>
2020-04-23 20:17:05 -0400

    Update 02_dwiQC.sh

--- 02_dwiQC.sh
+++ 02_dwiQC.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
+
 ####################################################################################
-#Name:         02_dwiQC.sh
+#Name:         #02_dwiQC.sh
 
-#Last updated: 2020-03-17
+#Last updated: #2020-03-17
 
-#Description:  This script checks the automated QC results produced by dwiprep. Even though 
-               I am using preprocessed outputs from dmriprep (in-house pipeline), the 
-               automated dwiprep QC outputs remain useful. Specifically, we check for the 
-               3 PASS results at the end of each participant's `*_QCReport.txt` file. All 
-               participants that PASS are included in subsequent analyses
+#Description:  
+               #This script checks the automated QC results produced by dwiprep. Even though 
+               #I am using preprocessed outputs from dmriprep (in-house pipeline), the 
+               #automated dwiprep QC outputs remain useful. Specifically, we check for the 
+               #3 PASS results at the end of each participant's `*_QCReport.txt` file. All 
+               #participants that PASS are included in subsequent analyses
 
-#Submission:   Run as bash or interactively in terminal
+#Submission:   #Run as bash or interactively in terminal
 
-#Notes:        If it exists, make sure to remove the `participants.csv` file that has been 
-               added in the archive, from 02_dwiprepQC_automatedFAILS.txt and 03_sublist.txt
-
+#Notes:        #If it exists, make sure to remove the `participants.csv` file that has been 
+               #added in the archive, from 02_dwiprepQC_automatedFAILS.txt and 03_sublist.txt
 ####################################################################################
 
 #move to dtiprep directory
