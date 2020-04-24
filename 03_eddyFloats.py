6d1a4d6cd4f9d8cd6a0237f9a638c19688cdeb61
ɐuoʌɐu <navona.calarco@camh.ca>
2020-04-23 21:10:06 -0400

    Update 03_eddyFloats.py

--- 03_eddyFloats.py
+++ 03_eddyFloats.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python
+
+####################################################################################
+#Name:         03_eddyFloats.py
+
+#Last updated: 2020-03-17
+
+#Description:  Remove decimal places from eddy-corrected images
+
+#Submission:   I run interactively in ipython (avoid spacing issues with python)
+
+#Notes: 
+               #We need to convert all of the eddy-corrected images to 'ints', 
+               #i.e., remove the decimal place, as dtiprep doesn't allow for floats (???)
+               #This code takes a bit of time to run -- about 6 seconds per participant -- so about an hour for SPINS  
+
+####################################################################################
+
+#modules
+module load python/3.8.1
+
+#use ipython interactively
+ipython
+
+#import packages
 import nibabel as nib
 import numpy as np
 import pandas as pd
 import os
 from glob import glob
 
-#We need to convert all of the eddy-corrected images to 'ints', and remove the decimal place
-#The reason for this is that dtiprep doesn't allow for decimal places that eddy produces???
-
-#Notes to self:
-#If running in the terminal, module load Python 3+, and then run in ipython
-#If getting errors about indents, may be because Python doesn't like spaces in the terminal
-#Because of difficulties installing dependencies in my own Python environment, I will use the lab's Python 
-#This code takes a bit of time to run -- about 6 seconds per participant -- so about an hour for SPINS 
-
-#module load python/3.8.1
-#ipython
-
 #make the directory for the output
 mkdir -p /projects/ncalarco/thesis/SPINS/Slicer/data/03_dmriprep_INT
 
+#define directory variables
 input_dir   = "/projects/ncalarco/thesis/SPINS/Slicer/data/01_dmriprep"
 output_dir  = "/projects/ncalarco/thesis/SPINS/Slicer/data/03_dmriprep_INT"
+
+#set up subject list
 sublist = open("/projects/ncalarco/thesis/SPINS/Slicer/txt_outputs/03_sublist.txt", "r")
 SID = sublist.readlines()
 SID = [i.replace('\n', '') for i in SID]
 
+#run conversion
 for i in SID:
     print(i)
     check_exists = os.path.join(output_dir, i, i + '_eddy_fixed.nii.gz')
