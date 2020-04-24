1fd29aa0f390a826cbdd6bf53811dc8862ef4479
ɐuoʌɐu <navona.calarco@camh.ca>
2020-04-23 21:14:37 -0400

    Update 04_makeNRRDs.sh

--- 04_makeNRRDs.sh
+++ 04_makeNRRDs.sh
@@ -1,22 +1,34 @@
-#We need to convert to .nrrds, as Slicer requires .nrrds to perform tractography
+#!/bin/bash
 
+####################################################################################
+#Name:         #04_makeNRRDs.sh
+
+#Last updated: #2020-03-17
+
+#Description:  #Convert .nii to .nrrd, as required by Slicer to perform tractography
+
+#Submission:   #bash
+
+#Notes:        #May need to follow up: do I need to convert Siemens data to .nrrd 
+               #differently than non-Siemens? Fan suggests may want to use 
+               #the --useBMatrixGradientDirections flag, but all looks ok...
+               #reference: http://dmri.slicer.org/tutorials/Slicer-4.8/DWIConverterTutorial.pdf
+####################################################################################
+
+#load modules
 module load slicer/0,nightly
 module load SGE-extras/1.0
 
+#define directory variables
 input_dir_dwi="/projects/ncalarco/thesis/SPINS/Slicer/data/03_dmriprep_INT"
-input_dir_masks="/projects/ncalarco/thesis/SPINS/Slicer/data/01_dmriprep"  #make sure no integer problem with masks
+input_dir_masks="/projects/ncalarco/thesis/SPINS/Slicer/data/01_dmriprep"  #m
 output_dir="/projects/ncalarco/thesis/SPINS/Slicer/data/04_nrrd"
 data_dir="/projects/ncalarco/thesis/SPINS/Slicer/data/01_dmriprep"
 
-#make output 04_nrrd directory
+#make output directory
 mkdir -p $output_dir
 
-####################################################################################
-#Note: Do I need to convert Siemens data to .nrrd differently than non-Siemens? 
-#Fan suggests may want to use --useBMatrixGradientDirections flag
-#reference: http://dmri.slicer.org/tutorials/Slicer-4.8/DWIConverterTutorial.pdf
-####################################################################################
-
+#run conversion with dwiconvert (native to Slicer)
 while read id; do
 echo ${id}
 if [ ! -e ${output_dir}/${id}_eddy_fixed.nrrd ]; then
