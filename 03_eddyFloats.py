98dd59a0130e9cd2e8052e799b61e81fb9e6c086
ɐuoʌɐu <navona.calarco@camh.ca>
2020-05-05 02:19:43 -0400

    Update 03_eddyFloats.py

--- 03_eddyFloats.py
+++ 03_eddyFloats.py
@@ -36,29 +36,16 @@ mkdir -p /projects/ncalarco/thesis/SPINS/Slicer/data/03_dmriprep_INT
 input_dir   = "/projects/ncalarco/thesis/SPINS/Slicer/data/01_dmriprep"
 output_dir  = "/projects/ncalarco/thesis/SPINS/Slicer/data/03_dmriprep_INT"
 
-#set up subject list
-sublist = open("/projects/ncalarco/thesis/SPINS/Slicer/txt_outputs/03_sublist.txt", "r")
-SID = sublist.readlines()
-SID = [i.replace('\n', '') for i in SID]
-
 #run conversion
-for i in SID:
-    print(i)
-    check_exists = os.path.join(output_dir, i, i + '_eddy_fixed.nii.gz')
+eddy_files = sorted(glob(f"{input_dir}/*nii.gz"))
+for f in eddy_files:
 
-    if not os.path.isfile(check_exists):
-        os.makedirs(os.path.join(output_dir, i))
-        sub_image = glob(os.path.join(input_dir, i, '*', '*', '*desc-brainsuite_dwi.nii.gz'))[0]
+    base_file = os.path.basename(f).replace("_desc-preproc_dwi.nii.gz", "")
+    output_file = os.path.join(output_dir, base_file + "_eddy_fixed.nii.gz")
 
-    if os.path.isfile(sub_image):
-        img = nib.load(sub_image)
-        hdr = img.header
-        new_data = np.copy(img.get_data())
-        new_dtype = np.int16
-        new_data = new_data.astype(new_dtype)
-        img.set_data_dtype(new_dtype)
-
-        new_image = nib.Nifti1Image(new_data, img.affine, header=hdr)
-        print(new_image.get_data_dtype())
-
-        nib.save(new_image, check_exists)
+    if not os.path.isfile(output_file):
+        img = nb.load(f)
+        data = img.get_fdata().astype(np.int16)
+        new_img = nb.Nifti1Image(data, img.affine, img.header)
+        new_img.header.set_data_dtype(np.int16)
+        new_img.to_filename(output_file)
